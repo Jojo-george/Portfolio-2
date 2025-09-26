6c03705cde1303f6bf9b7a46fd0924aa24efec5c
Jojo George <jojogeorge621gmail.com>
2025-09-26 15:07:22 +0530

    renamed html file

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app=Flask(__name__)
 
 @app.route("/")
 def portfolio():
-    return render_template("portfolio.html")
+    return render_template("port.html")
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",debug=True)
